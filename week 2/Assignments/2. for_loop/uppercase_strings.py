a3c0c9251f2310dfd4d9a6b6c62f50dd7925cd09
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2023-04-11 13:04:59 +0100

    week 2 - for loop assignments - uppercase_strings.py

---
+++ week 2/Assignments/2. for_loop/uppercase_strings.py
@@ -0,0 +1,7 @@
+# Create a for loop that iterates through a list
+# of strings and prints each string in uppercase
+
+strings = ["Anything", "Something", "Everything"]
+
+for string in strings:
+    print(string.upper())
