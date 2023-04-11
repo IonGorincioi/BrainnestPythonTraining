876903c5f12d451229db01714fbe64aa23e38ff8
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2023-04-11 12:16:53 +0100

    week 2 - while loop assignments - append_if_number.py

---
+++ week 2/Assignments/1. while_loop/append_if_number.py
@@ -0,0 +1,22 @@
+# Create a while loop that repeatedly takes user input and appends
+# it to a list, but only if the input is a number greater than 10.
+
+numbers = []
+end_of_loop = True
+status = ""
+print("Type 'done' to exit. ")
+while end_of_loop:
+    user_input = input("Enter an input: ")
+
+    if user_input.lower() != 'done':
+        user_input = user_input.strip()
+
+        if user_input.isnumeric():
+            user_input = int(user_input)
+            if user_input > 10:
+                numbers.append(user_input)
+    else:
+        end_of_loop = False
+
+    print(numbers)
+
