288ab27d69338a009a47502db93d76cf83caf502
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2023-04-17 13:49:12 +0100

    week 2 - project Euler - sum_of_primes.py updated

--- week 2/Assignments/3. project_euler/sum_of_primes.py
+++ week 2/Assignments/3. project_euler/sum_of_primes.py
@@ -2,16 +2,18 @@
 # Find the sum of all the primes below two million.
 
 def prime_numbers(max_number):
+
     number = 1
-    total = 2
+    primes = [2]
     while number < max_number:
         number += 2
         for divisor in range(2, max_number):
             if number % divisor == 0:
                 break
         if divisor == number:
-            total += number
+            primes.append(number)
+            total = sum(primes)
     return total
 
 
-print(prime_numbers(200000))
+print(prime_numbers(2000000))
