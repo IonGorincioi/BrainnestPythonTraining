ba5c9ace335c382576f23a8dd36b3621cbc79da9
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2023-04-20 12:47:37 +0100

    week 3 - classroom self-learning

--- week 3/TUE - 17-04-2023/test ReGex.py
+++ week 3/TUE - 17-04-2023/test ReGex.py
@@ -4,7 +4,6 @@ import re
 text = "I am 37 years old. I was born on 13-08-1984."
 text1 = "2023"
 
-
 pattern = r'\d+'
 # number = re.search(pattern, text)
 # number = re.match(pattern, text)
@@ -16,6 +15,10 @@ print('Starting index:', number.start())
 print('End position:', number.end())
 print('Position:', number.span())
 
+print("-------------------------------------------")
+s = 'Python 3.0 was released in 2008'
+matches = re.finditer(r'\W', s)
+for match in matches:
+    print(match.group())
 
-
-
+print("-------------------------------------------")
