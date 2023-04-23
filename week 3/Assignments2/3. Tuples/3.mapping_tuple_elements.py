caf0274333998d84086de052af5d7118771d65b6
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2023-04-23 19:57:33 +0100

    week 3 - Assignments 2 - Tuples - mapping_tuple_elements.py

--- week 3/Assignments2/3. Tuples/3.mapping_tuple_elements.py
+++ week 3/Assignments2/3. Tuples/3.mapping_tuple_elements.py
@@ -3,16 +3,27 @@
 
 def find_most_common_element(input_tuple):
     frequency = {}
+    result = {}
     for element in input_tuple:
-        element = element.capitalize()
+
+        # check whether the element in the tuple is a number
+        if not str(element).isnumeric():
+            element = element.capitalize()
+
+        # counts the frequency of each element
         if element in frequency:
             frequency[element] += 1
         else:
             frequency[element] = 1
 
-    for key, value in frequency.items():
-        print(key, ':', value)
+        # assigns "element : frequency" key-value pairs in dictionary
+        result[element] = frequency[element]
+
+    return result
 
 
-names = ("John", "Mary", "Ion", "Mary", "Michael", "iON")
+names = ("John", "Mary", "Ion", "maRy", "Michael", "iON")
+numbers = (11, 23, 4, 23, 5, 6, 23, 34, 33, 12, 23, 14)
+
 print(find_most_common_element(names))
+print(find_most_common_element(numbers))
