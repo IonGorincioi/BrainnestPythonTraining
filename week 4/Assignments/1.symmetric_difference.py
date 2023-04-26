41175067ca47296e859235fb9285e00cf58302dd
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2023-04-26 11:27:07 +0100

    week 4 - sets - symmetric_difference.py

---
+++ week 4/Assignments/1.symmetric_difference.py
@@ -0,0 +1,15 @@
+# Given two sets A and B, write a function to return
+# the symmetric difference between them as a new set.
+# The symmetric difference is the set of elements
+# that are in either A or B, but not in both.
+
+def sym_dif(s1, s2):
+    result = s1.symmetric_difference(s2)
+    return result
+
+
+set1 = {"javascript", "python", "C#"}
+set2 = {"html", "css", "javascript", 'C#'}
+
+symmetricDifference = sym_dif(set1, set2)
+print(symmetricDifference)
