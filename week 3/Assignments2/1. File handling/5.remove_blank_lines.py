dbe00796150038445d939b0c55bcc315022ee4cb
Ion Gorincioi <49958966+IonGorincioi@users.noreply.github.com>
2023-04-29 13:58:53 +0100

    week 3 - Assignments 2  - remove_blank_lines.py

--- week 3/Assignments2/1. File handling/5.remove_blank_lines.py
+++ week 3/Assignments2/1. File handling/5.remove_blank_lines.py
@@ -1,6 +1,11 @@
 # Write a Python program that reads a text file and removes all the blank lines.
 # The modified text should be written back to the file.
 
+"""
+NOTE:
+    Please check "file_with_blanks.txt" file
+    before and after execution to see the result
+"""
 
 with open("files/file_with_blanks.txt", "r") as file:
     content = file.readlines()
@@ -9,3 +14,5 @@ with open("files/file_with_blanks.txt", 'w') as file:
     for line in content:
         if not line.isspace():
             file.write(line)
+
+
